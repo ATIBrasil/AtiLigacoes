352eac4f95232da13b067cac11f805dbdc2bd5ed
ATIBrasil <informatica@atibrasil.com.br>
2024-01-08 12:35:39 -0300

    a

--- src/js/login.js
+++ src/js/login.js
@@ -20,6 +20,10 @@ export async function fazerLogin(callback) {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
+                "Access-Control-Allow-Credentials": "true",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+                "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
             },
             body: JSON.stringify({ login, password }),
         });
